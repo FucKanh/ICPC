8e7ba68f0399f6383b146bbe1ced27bb6a6811fc
Mạc Phúc Khang <95740446+Macphuckhang11112007@users.noreply.github.com>
2023-05-26 04:22:20 +0700

    Update Expression Add Operators.cpp

--- GeeksforGeeks/Expression Add Operators/Expression Add Operators.cpp
+++ GeeksforGeeks/Expression Add Operators/Expression Add Operators.cpp
@@ -10,40 +10,41 @@ class Solution {
 	  public:
 		#define ll long long
 		#define eb emplace_back
-    	vector <string> a;
-    	string S;
-    	ll x;
-    	void Backtrack(ll i = 0, ll sum = 0, ll s = 0, ll num = 0, ll mul = 0, string str = "", string sym = "")
-    	{
-        	if (i == S.size())
-        	{
-            	sum += s;
-            	if (sum == x)
-                	a.eb(str);
-        	}
-        	else
-        	{
-            	if (sym != "*")
-                	Backtrack(i + 1, sum, 10 * s + (s >= 0 ? 1 : -1) * (S[i] - '0'), 10 * s + (s >= 0 ? 1 : -1) * (S[i] - '0'), 10 * s + (s >= 0 ? 1 : -1) * (S[i] - '0'), str + S[i], sym);
-            	else
-                	Backtrack(i + 1, sum, mul * (10 * num + (S[i] - '0')), 10 * num + (S[i] - '0'), mul, str + S[i], sym);
-            	if (i && S[i] != '0')
-            	{
-                	Backtrack(i + 1, sum + s, S[i] - '0', S[i] - '0', S[i] - '0', str + "+" + S[i], "+");
-                	Backtrack(i + 1, sum, s * (S[i] - '0'), S[i] - '0', s, str + "*" + S[i], "*");
-                	Backtrack(i + 1, sum + s, -(S[i] - '0'), -(S[i] - '0'), -(S[i] - '0'), str + "-" + S[i], "-");
-            	}
-        	}
-        	return;
-    	}
-    	vector<string> addOperators(string str, int target) {
-        	// code here
-        	a.clear();
-        	S = str;
-        	x = target;
-        	Backtrack();
-        	return a;
-    	}
+    		vector <string> a;
+    		string S;
+    		ll x;
+    		void Backtrack(ll i = 0, ll sum = 0, ll s = 0, ll num = 0, ll mul = 0, string str = "", string sym = "")
+    		{
+        		if (i == S.size())
+        		{
+            			sum += s;
+            			if (sum == x)
+                		a.eb(str);
+        		}
+			else
+        		{
+            			if (sym != "*")
+                			Backtrack(i + 1, sum, 10 * s + (s >= 0 ? 1 : -1) * (S[i] - '0'), 10 * s + (s >= 0 ? 1 : -1) * (S[i] - '0'), 10 * s + (s >= 0 ? 1 : -1) * (S[i] - '0'), str + S[i], sym);
+            			else
+                			Backtrack(i + 1, sum, mul * (10 * num + (S[i] - '0')), 10 * num + (S[i] - '0'), mul, str + S[i], sym);
+            			if (i && S[i] != '0')
+            			{
+                			Backtrack(i + 1, sum + s, S[i] - '0', S[i] - '0', S[i] - '0', str + "+" + S[i], "+");
+                			Backtrack(i + 1, sum, s * (S[i] - '0'), S[i] - '0', s, str + "*" + S[i], "*");
+                			Backtrack(i + 1, sum + s, -(S[i] - '0'), -(S[i] - '0'), -(S[i] - '0'), str + "-" + S[i], "-");
+            			}
+        		}
+        		return;
+    		}
+    		vector<string> addOperators(string str, int target) 
+		{
+        		// code here
+        		a.clear();
+        		S = str;
+        		x = target;
+        		Backtrack();
+        		return a;
+    		}
 };
 
 //{ Driver Code Starts.
@@ -51,7 +52,8 @@ class Solution {
 int main() {
 		int t;
 		cin >> t;
-		while (t--) {
+		while (t--) 
+		{
 			int target;
 			string S;
 			cin >> S >> target;
