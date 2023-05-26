0c39250076c20d0f6de3cc8de950eec4275eaa4b
Mạc Phúc Khang <95740446+Macphuckhang11112007@users.noreply.github.com>
2023-05-26 23:14:34 +0700

    Update Allocate minimum number of pages.cpp

--- GeeksforGeeks/Dynamic Programming/Allocate minimum number of pages.cpp
+++ GeeksforGeeks/Dynamic Programming/Allocate minimum number of pages.cpp
@@ -14,36 +14,36 @@ class Solution
 {
     public:
     //Function to find minimum number of pages.
-    #define ll long long
-    ll findPages(int arr[], int n, int m)
+#define ll long long
+    ll findPages ( int arr [ ] , int n , int m )
     {
         //code here
-        if (m > n)
+        if ( m > n )
             return -1;
-        ll low = 0, high = 0;
-        for (int i = 1; i <= n; ++i)
+        ll low = 0 , high = 0;
+        for ( int i = 1; i <= n; ++i )
         {
-            low = max(low, (ll) arr[i - 1]);
-            high += arr[i - 1];
+            low = max ( low , ( ll ) arr [ i - 1 ] );
+            high += arr [ i - 1 ];
         }
-        while (low < high)
+        while ( low < high )
         {
-            ll mid = (low + high) / 2;
-            ll s = 0, cnt = 1;
+            ll mid = ( low + high ) / 2;
+            ll s = 0 , cnt = 1;
             bool chk = 1;
-            for (int i = 1; i <= n && chk; ++i)
+            for ( int i = 1; i <= n && chk; ++i )
             {
-                if (arr[i - 1] <= mid)
-                    s += arr[i - 1];
-                if (s > mid)
+                if ( arr [ i - 1 ] <= mid )
+                    s += arr [ i - 1 ];
+                if ( s > mid )
                 {
                     ++cnt;
-                    s = arr[i - 1];
+                    s = arr [ i - 1 ];
                 }
-                if (arr[i - 1] > mid || cnt > m)
+                if ( arr [ i - 1 ] > mid || cnt > m )
                     chk = 0;
             }
-            if (chk)
+            if ( chk )
                 high = mid;
             else
                 low = mid + 1;
@@ -54,20 +54,23 @@ class Solution
 
 //{ Driver Code Starts.
 
-int main() {
+int main ( )
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while ( t-- )
+    {
         int n;
-        cin>>n;
-        int A[n];
-        for(int i=0;i<n;i++){
-            cin>>A[i];
+        cin >> n;
+        int A [ n ];
+        for ( int i = 0; i < n; i++ )
+        {
+            cin >> A [ i ];
         }
         int m;
-        cin>>m;
+        cin >> m;
         Solution ob;
-        cout << ob.findPages(A, n, m) << endl;
+        cout << ob.findPages ( A , n , m ) << endl;
     }
     return 0;
 }
