134669c09b621c7fac2f2e400ab088ec30f9ccb9
Mạc Phúc Khang <95740446+Macphuckhang11112007@users.noreply.github.com>
2023-05-26 23:16:22 +0700

    Update Maximum difference of zeros and ones in binary string.cpp

--- GeeksforGeeks/Dynamic Programming/Maximum difference of zeros and ones in binary string.cpp
+++ GeeksforGeeks/Dynamic Programming/Maximum difference of zeros and ones in binary string.cpp
@@ -7,43 +7,44 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
-class Solution{
-public:
-	int maxSubstring(string S)
-	{
-	    // Your code goes here
-	    int ans = -1e9, s = 0;
-	    for (int i = 1; i <= S.size(); ++i)
-	    {
-	        s += (S[i - 1] == '0' ? 1 : -1);
-	        ans = max(ans, s);
-	        s = max(s, 0);
-	    }
-	    return ans;
-	}
+class Solution
+{
+    public:
+    int maxSubstring ( string S )
+    {
+        // Your code goes here
+        int ans = -1e9 , s = 0;
+        for ( int i = 1; i <= S.size ( ); ++i )
+        {
+            s += ( S [ i - 1 ] == '0' ? 1 : -1 );
+            ans = max ( ans , s );
+            s = max ( s , 0 );
+        }
+        return ans;
+    }
 };
 
 //{ Driver Code Starts.
 
-int main()
+int main ( )
 {
 
 
-   	ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio ( 0 );
+    cin.tie ( NULL );
+    cout.tie ( NULL );
 
-   	int t;
-   	cin >> t;
-   	while(t--)
-   	{
-   		string s;
-   		cin >> s;
+    int t;
+    cin >> t;
+    while ( t-- )
+    {
+        string s;
+        cin >> s;
 
-   	    Solution ob;
+        Solution ob;
 
-   		cout << ob.maxSubstring(s) << "\n";
-   	}
+        cout << ob.maxSubstring ( s ) << "\n";
+    }
 
     return 0;
 }
