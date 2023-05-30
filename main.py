534af079096fe3d64b18b963dded3a7a377aaab5
FubuGold <trangiahuy211207@gmail.com>
2023-05-30 20:48:35 +0700

    no login

--- main.py
+++ main.py
@@ -31,43 +31,43 @@ def main():
         )
     
 
-    name, authentication_status, username = auth.login('Login', 'main')
+    # name, authentication_status, username = auth.login('Login', 'main')
 
-    if st.session_state["authentication_status"] is None:
-        st.write("Please enter username and password")
-        try:
-            username_forgot_pw, email_forgot_password, random_password = auth.forgot_password('Forgot password','sidebar')
-            if username_forgot_pw:
-                st.success('New password sent securely. Check your email')
-            else:
-                st.error('Username not found')
-        except Exception as e:
-            st.error(e)
+    # if st.session_state["authentication_status"] is None:
+    #     st.write("Please enter username and password")
+    #     try:
+    #         username_forgot_pw, email_forgot_password, random_password = auth.forgot_password('Forgot password','sidebar')
+    #         if username_forgot_pw:
+    #             st.success('New password sent securely. Check your email')
+    #         else:
+    #             st.error('Username not found')
+    #     except Exception as e:
+    #         st.error(e)
 
-        try:
-            username_forgot_username, email_forgot_username = auth.forgot_username('Forgot username','sidebar')
-            if username_forgot_username:
-                st.success('Username sent securely')
+    #     try:
+    #         username_forgot_username, email_forgot_username = auth.forgot_username('Forgot username','sidebar')
+    #         if username_forgot_username:
+    #             st.success('Username sent securely')
 
-            else:
-                st.error('Email not found')
-        except Exception as e:
-            st.error(e)
+    #         else:
+    #             st.error('Email not found')
+    #     except Exception as e:
+    #         st.error(e)
     
-    elif st.session_state["authentication_status"] is False:
-        st.error('Incorrect username/password')
+    # elif st.session_state["authentication_status"] is False:
+    #     st.error('Incorrect username/password')
 
-    elif st.session_state["authentication_status"]:
-        post_authenticate(auth,username)
+    # elif st.session_state["authentication_status"]:
+        # post_authenticate(auth,username)
 
-        upload_and_search = uploadNsearch(name)
-        code_search,code_upload = st.tabs(['Search code','Submit code'])
+    upload_and_search = uploadNsearch('huy')
+    code_search,code_upload = st.tabs(['Search code','Submit code'])
 
-        with code_search:
-            upload_and_search.codeSearch()
+    with code_search:
+        upload_and_search.codeSearch()
 
-        with code_upload:
-            upload_and_search.uploadCode()
+    with code_upload:
+        upload_and_search.uploadCode()
         
 
 if __name__ == '__main__':
